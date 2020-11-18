091bb20cec6624b6114684d04cd78e2b35ab9d2e
CorvusVibeZ <CorvusVibeZ@users.noreply.github.com>
2020-11-18 13:50:32 +0100

    Erster Commit, Erstellung der Packages zur Realisation der verschiedenen Layer.

---
+++ src/main/java/com/SpringBootAssignment/SpringBootAssignmentLandApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAssignmentLandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAssignmentLandApplication.class, args);
+	}
+
+}
