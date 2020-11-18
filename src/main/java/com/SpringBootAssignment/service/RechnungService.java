bfaf304c6022ce77481e5b7f3d2b17a5032a0af6
CorvusVibeZ <CorvusVibeZ@users.noreply.github.com>
2020-11-18 14:34:20 +0100

    Fünfter Commit, die RechnungService Klasse wurde implementiert.

--- src/main/java/com/SpringBootAssignment/service/RechnungService.java
+++ src/main/java/com/SpringBootAssignment/service/RechnungService.java
@@ -1,4 +1,41 @@
 package com.SpringBootAssignment.service;
 
+
+import com.SpringBootAssignment.model.Rechnung;
+import com.SpringBootAssignment.repo.RechnungsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RechnungService {
+
+    @Autowired
+    private RechnungsRepository repo;
+
+    public List<Rechnung> listAll() {
+
+        return repo.findAll();
+    }
+
+    public void save(Rechnung rechnung) {
+
+        repo.save(rechnung);
+    }
+
+    public Rechnung get(Long id) {
+
+        return repo.findById(id).get();
+    }
+
+    public void delete(Long id) {
+        repo.deleteById(id);
+
+    }
+
+
+
+
+
 }
