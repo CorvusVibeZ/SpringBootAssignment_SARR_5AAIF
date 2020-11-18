93fee92ce455967ff9700dc098f3e61f98f9fd48
CorvusVibeZ <CorvusVibeZ@users.noreply.github.com>
2020-11-18 14:18:56 +0100

    Dritter Commit, application properties wurden für die H2 Datenbank implementiert. Die KundenRepository wurde fertig implementiert und es wurden Service Klassen für das Projekt erstellt.

--- src/main/java/com/SpringBootAssignment/model/Kunde.java
+++ src/main/java/com/SpringBootAssignment/model/Kunde.java
@@ -25,4 +25,36 @@ public class Kunde {
     @OneToMany
     private List<Rechnung> rechnungsliste;
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public List<Rechnung> getRechnungsliste() {
+        return rechnungsliste;
+    }
+
+    public void setRechnungsliste(List<Rechnung> rechnungsliste) {
+        this.rechnungsliste = rechnungsliste;
+    }
 }
--- src/main/java/com/SpringBootAssignment/repo/KundeRepository.java
+++ src/main/java/com/SpringBootAssignment/repo/KundeRepository.java
@@ -1,9 +1,15 @@
 package com.SpringBootAssignment.repo;
 
 
+import com.SpringBootAssignment.model.Kunde;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface KundeRepository extends JpaRepository {
+public interface KundeRepository extends JpaRepository<Kunde,Long> {
+
+
 }
